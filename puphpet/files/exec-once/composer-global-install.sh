8a07867facc4a6166771effa30ee0b6cd0ad16b3
Jad BITAR <jad@Jads-Mac-Pro.local>
2014-07-07 12:16:20 -0400

    Add PHP QA tools.

---
+++ puphpet/files/exec-once/composer-global-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+composer global require "phpunit/phpunit=4.1.*"
+composer global require 'phploc/phploc=*'
+composer global require 'phpmd/phpmd=*'
+composer global require 'squizlabs/php_codesniffer=*'
+composer global require 'sebastian/phpcpd=*'
+composer global require 'sebastian/phpdcd=*'
